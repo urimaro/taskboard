e7e0abf25f64caad5be026975314b88280a893c4
Yuki Yagi <y-yagi@froide.co.jp>
2017-04-06 11:23:23 +0900

    Todo/Doing/Done の各項目表示をテスト

--- spec/feature/index_spec.rb
+++ spec/feature/index_spec.rb
@@ -1,15 +1,28 @@
 require 'spec_helper'
 
 describe 'loginページ' do
-  specify 'スイムレーンの表示' do
+  before :each do
     visit '/'
+    sleep 5
+  end
+  specify 'スイムレーンの表示' do
     expect(page).to have_selector('.swimlane h1', text: 'Todo')
     expect(page).to have_selector('.swimlane h1', text: 'Doing')
     expect(page).to have_selector('.swimlane h1', text: 'Done')
   end
 
   specify 'Todoの表示' do
-    visit '/'
-    expect(page).to have_selector('div', text: 'Todoのサンプル1')
+    expect(page).to have_selector('#todo div.postit div', text: 'Todoのサンプル1')
+    expect(page).to have_selector('#todo div.postit div', text: 'Todoのサンプル2')
+  end
+
+  specify 'Doingの表示' do
+    expect(page).to have_selector('#doing div.postit div', text: 'Doingのサンプル1')
+    expect(page).to have_selector('#doing div.postit div', text: 'Doingのサンプル2')
+  end
+
+  specify 'Doneの表示' do
+    expect(page).to have_selector('#done div.postit div', text: 'Doneのサンプル1')
+    expect(page).to have_selector('#done div.postit div', text: 'Doneのサンプル2')
   end
 end
